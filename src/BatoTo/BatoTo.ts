c8394e1db078025fe5b9c92527256e326328545c
Marishka NM <marishkanmagness@gmail.com>
2025-12-17 04:53:47 +1100

    fixed versioning and title

--- src/BatoTo/BatoTo.ts
+++ src/BatoTo/BatoTo.ts
@@ -43,8 +43,8 @@ import { languageSettings,
 const BATO_DOMAIN_DEFAULT = BTDomains.getDefault()
 
 export const BatoToInfo: SourceInfo = {
-    version: '3.1.6',
-    name: 'BatoTo Dev Alt',
+    version: '3.1.7',
+    name: 'BatoTo Dynamic Domain',
     icon: 'icon.png',
     author: 'niclimcy',
     authorWebsite: 'https://github.com/niclimcy',
