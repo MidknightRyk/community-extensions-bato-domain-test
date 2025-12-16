8a35ab85b25012ad32ba6e4736f6d1f9be335cfd
Marishka NM <marishkanmagness@gmail.com>
2025-12-17 08:50:41 +1100

    incremented test ver

--- src/BatoTo/BatoTo.ts
+++ src/BatoTo/BatoTo.ts
@@ -44,7 +44,7 @@ const BATO_DOMAIN_DEFAULT = BTDomains.getDefault()
 
 export const BatoToInfo: SourceInfo = {
     version: '3.1.7',
-    name: 'BatoTo Dynamic Domain testing',
+    name: 'BatoTo Dynamic Domain test2',
     icon: 'icon.png',
     author: 'niclimcy',
     authorWebsite: 'https://github.com/niclimcy',
