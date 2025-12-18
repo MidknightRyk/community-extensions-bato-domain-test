83291b89499152e41b1ba612b43eb84f9ee7dbbb
Marishka NM <marishkanmagness@gmail.com>
2025-12-18 15:19:39 +1100

    ver bump

--- src/BatoTo/BatoTo.ts
+++ src/BatoTo/BatoTo.ts
@@ -44,7 +44,7 @@ const BATO_DOMAIN_DEFAULT = 'https://bato.to'
 
 export const BatoToInfo: SourceInfo = {
     version: '3.1.7',
-    name: 'BatoTo Dynamic Domain test6.5',
+    name: 'BatoTo Dynamic Domain test7',
     icon: 'icon.png',
     author: 'niclimcy',
     authorWebsite: 'https://github.com/niclimcy',
