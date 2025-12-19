df0fca74d196caf7e6bc25f3e05d84ad18dbc415
MidknightRyk <42915624+MidknightRyk@users.noreply.github.com>
2025-12-19 06:35:53 +0000

    Deploying to gh-pages from @ MidknightRyk/community-extensions-bato-domain-test@023185c8015cbd112584c3f5a15389eb0fb150da 🚀

--- 0.8-dynamic-domain/BatoTo/source.js
+++ 0.8-dynamic-domain/BatoTo/source.js
@@ -3265,7 +3265,7 @@ var _Sources = (() => {
   var BATO_DOMAIN_DEFAULT = BTDomains.getDefault()[0] ?? "https://bato.to";
   var BatoToInfo = {
     version: "3.1.7",
-    name: "BatoTo Dynamic 1.0",
+    name: "BatoTo Dynamic 1.1",
     icon: "icon.png",
     author: "niclimcy",
     authorWebsite: "https://github.com/niclimcy",
